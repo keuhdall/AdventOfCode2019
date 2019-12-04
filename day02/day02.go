3df4327b0dc23d646b021763d39e68a7bffccfe6
Leonard Marques <lmarques@mediarithmics.com>
2019-12-04 11:59:34 +0100

    part2 was working; cleanned code

--- day02/day02.go
+++ day02/day02.go
@@ -11,17 +11,17 @@ import (
 const filename = "input"
 
 func parseArray(arr []int) []int {
-	for i := 0; i < len(arr); i+=4 {
+	for i := 0; i < len(arr); i += 4 {
 		switch arr[i] {
-			case 1:
-				arr[arr[i+3]] = arr[arr[i+1]] + arr[arr[i+2]]
-			case 2:
-				arr[arr[i+3]] = arr[arr[i+1]] * arr[arr[i+2]]
-			case 99:
-				return arr
-			default:
-				fmt.Println("Unexpected opcode, exiting")
-				os.Exit(1)
+		case 1:
+			arr[arr[i+3]] = arr[arr[i+1]] + arr[arr[i+2]]
+		case 2:
+			arr[arr[i+3]] = arr[arr[i+1]] * arr[arr[i+2]]
+		case 99:
+			return arr
+		default:
+			fmt.Println("Unexpected opcode, exiting")
+			os.Exit(1)
 		}
 	}
 	return []int{0}
@@ -43,15 +43,14 @@ func convertArray(arr []string) []int {
 func searchGoal(arr []int, goal int) {
 	for i := 0; i < 99; i++ {
 		for j := 0; j < 99; j++ {
-			var mem = make([]int,len(arr))
-			copy(mem,arr)
+			var mem = make([]int, len(arr))
+			copy(mem, arr)
 			mem[1] = i
 			mem[2] = j
 			if parseArray(mem)[0] == goal {
-				fmt.Printf("Goal is %d %d\n", i,j)
+				fmt.Printf("Goal is %d %d\n", i, j)
 				return
 			}
-			mem = arr
 		}
 	}
 }
