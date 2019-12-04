8039c45cbd9f3e6b9b2167424405e7f5e128892f
Léonard Marques <lmarques@student.42.fr>
2019-12-04 03:02:03 +0100

    finished day02 part1

--- day02/day02.go
+++ day02/day02.go
@@ -11,32 +11,32 @@ import (
 const filename = "input"
 
 func parseArray(arr []int) {
-	for i := 0; i < len(arr); i++ {
+	for i := 0; i < len(arr); i+=4 {
 		switch arr[i] {
-		case 1:
-			arr[i+3] = arr[i+1] + arr[i+2]
-			break
-		case 2:
-			arr[i+3] = arr[i+1] * arr[i+2]
-			break
-		case 99:
-			fmt.Printf("Final value at index 0 : %d\n", arr[0])
-			return
-		default:
-			continue
+			case 1:
+				arr[arr[i+3]] = arr[arr[i+1]] + arr[arr[i+2]]
+			case 2:
+				arr[arr[i+3]] = arr[arr[i+1]] * arr[arr[i+2]]
+			case 99:
+				fmt.Println("Final state of the array : ")
+				fmt.Println(arr)
+				return
+			default:
+				fmt.Println("Unexpected opcode, exiting")
+				os.Exit(1)
 		}
 	}
 }
 
 func convertArray(arr []string) []int {
 	var intValues []int
-	for i := 0; i < len(arr); i++ {
-		val, err := strconv.Atoi(arr[i])
+	for _, i := range arr {
+		val, err := strconv.Atoi(i)
 		if err != nil {
 			fmt.Println(err.Error())
 			os.Exit(1)
 		}
-		intValues = append(intValues, i, val)
+		intValues = append(intValues, val)
 	}
 	return intValues
 }
