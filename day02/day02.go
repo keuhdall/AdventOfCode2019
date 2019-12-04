a5016945754baa534043624a519803fcb43564c0
Léonard Marques <lmarques@student.42.fr>
2019-12-04 04:17:37 +0100

    started working on part2

--- day02/day02.go
+++ day02/day02.go
@@ -10,7 +10,7 @@ import (
 
 const filename = "input"
 
-func parseArray(arr []int) {
+func parseArray(arr []int) []int {
 	for i := 0; i < len(arr); i+=4 {
 		switch arr[i] {
 			case 1:
@@ -18,14 +18,13 @@ func parseArray(arr []int) {
 			case 2:
 				arr[arr[i+3]] = arr[arr[i+1]] * arr[arr[i+2]]
 			case 99:
-				fmt.Println("Final state of the array : ")
-				fmt.Println(arr)
-				return
+				return arr
 			default:
 				fmt.Println("Unexpected opcode, exiting")
 				os.Exit(1)
 		}
 	}
+	return []int{0}
 }
 
 func convertArray(arr []string) []int {
@@ -41,6 +40,22 @@ func convertArray(arr []string) []int {
 	return intValues
 }
 
+func searchGoal(arr []int, goal int) {
+	for i := 0; i < 99; i++ {
+		for j := 0; j < 99; j++ {
+			var mem = make([]int,len(arr))
+			copy(mem,arr)
+			mem[1] = i
+			mem[2] = j
+			if parseArray(mem)[0] == goal {
+				fmt.Printf("Goal is %d %d\n", i,j)
+				return
+			}
+			mem = arr
+		}
+	}
+}
+
 func main() {
 	f, err := ioutil.ReadFile(filename)
 	if err != nil {
@@ -48,5 +63,5 @@ func main() {
 		os.Exit(1)
 	}
 	arr := convertArray(strings.Split(string(f), ","))
-	parseArray(arr)
+	searchGoal(arr, 19690720)
 }
